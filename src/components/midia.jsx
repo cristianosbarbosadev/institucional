08726d9f9eb5651ff64ef8cd705d84e6498022c6
Cristiano Barbosa <cristianosbarbosa.dev@gmail.com>
2023-06-09 02:18:13 -0300

    ajustes de link e SEO

---
+++ src/components/midia.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Midia = (props) => {
+  return (
+    <div id="midia" className="text-center">
+      <div className="container">
+        <div className="col-md-10 col-md-offset-1 section-title">
+          <h2>Publicidade em Caixa de Pizza</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
